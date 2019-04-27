2464a4f4a2a34d5d534dc144e90b95906b92c9a1
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-04-27 16:11:13 -0300

    Reformated code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ curr_bet = float(base_bet)
 side = "null"
 
 while(side != "ct" or side != "t"):
-	side = input("Choose your side [ct/t] ").replace("\n", "")
+    side = input("Choose your side [ct/t] ").replace("\n", "")
 
 bet_count = 0
 curr_money = 0
@@ -26,65 +26,71 @@ usr_name = ""
 usr_pass = ""
 
 with open("user_info.txt", "r") as usr_info:
-	usr_name = str(usr_info.readline()).replace("\n", "")
-	usr_pass = str(usr_info.readline()).replace("\n", "")
+    usr_name = str(usr_info.readline()).replace("\n", "")
+    usr_pass = str(usr_info.readline()).replace("\n", "")
 
 
 try:
-	driver.find_element_by_class_name("c-nav-buttons__login").click()
-	driver.find_element_by_name("username").send_keys(usr_name)
-	driver.find_element_by_name("password").send_keys(usr_pass)
+    driver.find_element_by_class_name("c-nav-buttons__login").click()
+    driver.find_element_by_name("username").send_keys(usr_name)
+    driver.find_element_by_name("password").send_keys(usr_pass)
 except Exception as e:
-	pass
+    pass
 input()
 
 curr_money = driver.find_element_by_css_selector("span.u-hl-gold").text
 
 while True:
-	time.sleep(1)
-	try:
-		driver.find_element_by_css_selector(".rolling-indicator")
+    time.sleep(1)
+    try:
+        driver.find_element_by_css_selector(".rolling-indicator")
 
-		driver.find_element_by_css_selector("button.c-radio-group__item:nth-child(1)").click()
-		driver.find_element_by_class_name("bet-amount").send_keys(str(curr_bet))
-		driver.find_element_by_class_name(side).click()
-		print("\n--------------------------------------------------------------------------------")
-		print(Fore.YELLOW + "Betted " +  str(curr_bet) + " on " + str(side) + Fore.RESET)
+        driver.find_element_by_css_selector(
+            "button.c-radio-group__item:nth-child(1)").click()
+        driver.find_element_by_class_name(
+            "bet-amount").send_keys(str(curr_bet))
+        driver.find_element_by_class_name(side).click()
+        print("\n--------------------------------------------------------------------------------")
+        print(Fore.YELLOW + "Betted " + str(curr_bet) +
+              " on " + str(side) + Fore.RESET)
 
-		t = driver.find_element_by_css_selector(".rolling-overlay__time > span:nth-child(1)").text
-		t = int(t) + 10
-		print("Waiting to roll - ", t)
+        t = driver.find_element_by_css_selector(
+            ".rolling-overlay__time > span:nth-child(1)").text
+        t = int(t) + 10
+        print("Waiting to roll - ", t)
 
-		time.sleep(t)
+        time.sleep(t)
 
-		try:
-			driver.find_element_by_css_selector("div.popup:nth-child(9) > p:nth-child(5) > label:nth-child(1) > i:nth-child(2)").click()
-			driver.find_element_by_css_selector("div.popup:nth-child(9) > div:nth-child(4) > a:nth-child(1)").click()
-		except Exception as identifier:
-			pass
+        try:
+            driver.find_element_by_css_selector(
+                "div.popup:nth-child(9) > p:nth-child(5) > label:nth-child(1) > i:nth-child(2)").click()
+            driver.find_element_by_css_selector(
+                "div.popup:nth-child(9) > div:nth-child(4) > a:nth-child(1)").click()
+        except Exception as identifier:
+            pass
 
-		buf = driver.find_element_by_css_selector("span.u-hl-gold").text
-		if (buf >= curr_money):
-			curr_bet = float(base_bet)
-			loses = 0
-			print(Fore.GREEN + "Win" + Fore.RESET)
-		else:
-			curr_bet = float(float(curr_bet) * 2)
-			loses = loses + 1
-			print(Fore.RED + "Lose" + Fore.RESET)
-		if (loses >= 6):
-			loses = 0
-			curr_bet = float(base_bet)
-		curr_money = buf
-		print("Finished")
-		bet_count += 1
-		print("Betted times ", str(bet_count))
-		if(bet_count >= 5):
-			driver.refresh()
-			bet_count = 0
-		print("Current balance: " + Fore.YELLOW + str(curr_money) + Fore.RESET)
-		print("--------------------------------------------------------------------------------")
-		continue
+        buf = driver.find_element_by_css_selector("span.u-hl-gold").text
+        if (buf >= curr_money):
+            curr_bet = float(base_bet)
+            loses = 0
+            print(Fore.GREEN + "Win" + Fore.RESET)
+        else:
+            curr_bet = float(float(curr_bet) * 2)
+            loses = loses + 1
+            print(Fore.RED + "Lose" + Fore.RESET)
+        if (loses >= 6):
+            loses = 0
+            curr_bet = float(base_bet)
+        curr_money = buf
+        print("Finished")
+        bet_count += 1
+        print("Betted times ", str(bet_count))
+        if(bet_count >= 5):
+            driver.refresh()
+            bet_count = 0
+        print("Current balance: " + Fore.YELLOW + str(curr_money) + Fore.RESET)
+        print("--------------------------------------------------------------------------------")
+        continue
 
-	except Exception as identifier:
-		continue
+    except Exception as identifier:
+        continue
