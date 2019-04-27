d97bd771f8ad1f2411e9f960556e68d44fadd45b
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-04-27 15:34:31 -0300

    Created setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Better',
+      version='0.2',
+      description='A bet bot',
+      packages=['colorama', 'seleinum'],
+			)
